05436b45a61ff468687f0cb34fa853a95dc7416f
a-anvarbek <c1221sharp@gmail.com>
2025-12-29 16:38:12 +0500

    feat: First commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+
+function App() {
+  // const [selectedServices, setSelectedServices] = useState<SelectedService[]>(
+  //   []
+  // );
+
+  return (
+    <>
+      <div>
+        <header></header>
+      </div>
+    </>
+  );
+}
+
+export default App;
